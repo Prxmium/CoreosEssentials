b44cc2dfd52c1b4ecda93ae49e3327e87ca02d4b
Prxmium <blazeitbotfilms@gmail.com>
2019-11-16 16:37:39 -0600

    added feed command

---
+++ src/com/gmail/corysaucer/Commands/FeedCommand.java
@@ -0,0 +1,29 @@
+package com.gmail.corysaucer.Commands;
+
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class FeedCommand {
+	
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		Player player = (Player) sender;
+		if(sender instanceof Player) {
+			player.hasPermission("coreo.feed");
+			player.setFoodLevel(20);
+			player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
+			return true;
+		}else{
+			sender.sendMessage(ChatColor.RED + "You don't meet the conditions required to run this command.");	
+			return true;
+			
+		}
+			
+		
+		
+	}
+
+}
