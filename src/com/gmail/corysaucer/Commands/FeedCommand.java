5313c6a36fc5fea2aa9b8a138b1884fecb4e4673
Prxmium <blazeitbotfilms@gmail.com>
2019-11-16 16:42:17 -0600

    forgot to implement CommandExecutor - added

--- src/com/gmail/corysaucer/Commands/FeedCommand.java
+++ src/com/gmail/corysaucer/Commands/FeedCommand.java
@@ -2,12 +2,13 @@ package com.gmail.corysaucer.Commands;
 
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import net.md_5.bungee.api.ChatColor;
 
-public class FeedCommand {
+public class FeedCommand implements CommandExecutor {
 	
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		Player player = (Player) sender;
