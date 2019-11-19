960a10121a03dafc013b9da24da7db7cde2264db
Prxmium <blazeitbotfilms@gmail.com>
2019-11-19 03:14:51 -0600

    added spectator mode command

---
+++ src/com/gmail/corysaucer/Commands/SpectatorCommand.java
@@ -0,0 +1,47 @@
+package com.gmail.corysaucer.Commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class SpectatorCommand implements CommandExecutor {
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if(sender instanceof Player) {
+		Player player = (Player) sender;
+			if (player.hasPermission("coreo.spectator")) {
+				if(args.length == 0) {
+					command.getName().equalsIgnoreCase("gmsp");
+					player.setGameMode(GameMode.SPECTATOR);
+					player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
+			
+					}else if(args.length == 1) {
+						Player target = Bukkit.getPlayer(args[0]);
+						if (target != null) {
+							target.setGameMode(GameMode.SPECTATOR);
+							player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
+					
+						}else{
+							sender.sendMessage(ChatColor.RED + "Player not found.");
+				}
+				
+			
+				}
+			
+			}
+			
+			
+			
+		}
+			
+		return true;
+	
+	}
+
+}
