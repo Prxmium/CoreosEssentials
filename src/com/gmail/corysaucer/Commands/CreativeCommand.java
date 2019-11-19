4f067e1bdd630393aa59052aa710132b88b559fa
Prxmium <blazeitbotfilms@gmail.com>
2019-11-19 03:03:43 -0600

    gmc can now be used on other players

--- src/com/gmail/corysaucer/Commands/CreativeCommand.java
+++ src/com/gmail/corysaucer/Commands/CreativeCommand.java
@@ -1,7 +1,9 @@
 package com.gmail.corysaucer.Commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,14 +13,28 @@ public class CreativeCommand implements CommandExecutor {
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		Player player = (Player) sender;
 		if(sender instanceof Player) {
-			player.hasPermission("coreo.creative");
+		Player player = (Player) sender;
+			if (player.hasPermission("coreo.creative")) {
+				if(args.length == 0) {
 			command.getName().equalsIgnoreCase("gmc");
-			player.setGameMode(GameMode.CREATIVE);
-		}else{
-			sender.sendMessage(ChatColor.RED + "You don't meet the conditions required to run this command.");
+			player.setGameMode(GameMode.CREATIVE);	
+			player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
+			
+			}else if(args.length == 1) {
+			Player target = Bukkit.getPlayer(args[0]);
+			if (target != null) {
+			target.setGameMode(GameMode.CREATIVE);
+			player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
+			}else{
+				player.sendMessage(ChatColor.RED + "Player not found.");
+			}	
+			}
+			
+			
+			}
 		}
+			
 		
 		return true;
 	
